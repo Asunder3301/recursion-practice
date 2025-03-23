6935b9065445548bbd37e0c3929206c86fff69c0
Asunder <trashemail3301@gmail.com>
2025-03-22 16:32:17 -0800

    Build: fibonacci
    added two functions to create an array containing the numbers of the Fibonacci sequence using an input. One is non-recursive and one is recursive.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-//Webpack test
 import './styles.css';
 
-console.log('Hello World!');
+//Fibonacci code
+
+function fibs(n) {
+    if (n <= 0) {
+      return [];
+    } else if (n === 1) {
+      return [0];
+    } else if (n === 2) {
+      return [0, 1];
+    }
+  
+    const fibArray = [0, 1];
+    for (let i = 2; i < n; i++) {
+      fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+    }
+    return fibArray;
+};
+
+function fibsRec(n) {
+    if (n <= 0) {
+      return [];
+    } else if (n === 1) {
+      return [0];
+    } else if (n === 2) {
+      return [0, 1];
+    } else {
+      const prevFib = fibs(n - 1);
+      return [...prevFib, prevFib[prevFib.length - 1] + prevFib[prevFib.length - 2]];
+    }
+};
+
+
+console.log(fibs(5));
+console.log(fibsRec(5));
