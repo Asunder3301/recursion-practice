26b3f6ed0aae5efcc16240ba02f9f908915166c0
Asunder <trashemail3301@gmail.com>
2025-03-23 09:06:07 -0800

    Build: merge sort
    created merge sort function to sort through an array and return the sorted array

--- src/index.js
+++ src/index.js
@@ -31,6 +31,38 @@ function fibsRec(n) {
     }
 };
 
+//Merge sort code
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
 
 console.log(fibs(5));
 console.log(fibsRec(5));
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
